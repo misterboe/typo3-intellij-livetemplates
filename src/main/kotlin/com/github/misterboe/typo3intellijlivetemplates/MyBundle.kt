e9cdbee8b565225298453ec1251ee24166579891
GitHub Action <action@github.com>
2023-12-20 08:19:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/misterboe/typo3intellijlivetemplates/MyBundle.kt
+++ src/main/kotlin/com/github/misterboe/typo3intellijlivetemplates/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.misterboe.typo3intellijlivetemplates
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/misterboe/typo3intellijlivetemplates/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/misterboe/typo3intellijlivetemplates/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.misterboe.typo3intellijlivetemplates.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
